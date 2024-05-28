8a2fa202edf9e2e22ea18db319e92debc7828d7f
Joseph Rodriguez <jrodri@mit.edu>
2024-05-28 11:19:22 -0400

    homepage touches

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,24 @@
+import React from 'react';
+import './Home.css';  // Import the CSS file for styling
 import ctaImage from './images/cta.png';  // importing the image
 import mitImage from './images/mit.png';  // importing the image
 import neuImage from './images/neu.png';  // importing the image
 
 const Home = () => {
-
   return (
     <div className="home">
       <h2>RailOps App</h2>
-        <h4 className="homepageText">
-            Welcome to the RailOps App! Click on stations above to get monitoring information and recommendations.
-            Developers: Zhihao Lyu, Joseph Rodriguez, Mojtaba Yousefi, Haris Koutsopoulos
-        </h4>
+      <h4 className="homepageText">
+        Welcome to the RailOps App! Click on stations above to get monitoring information and recommendations.
+      </h4>
+      <div className="images">
         <img src={ctaImage} alt="CTA Logo" />  {/* inserting the image */}
         <img src={mitImage} alt="MIT Logo" />  {/* inserting the image */}
         <img src={neuImage} alt="NEU Logo" />  {/* inserting the image */}
-        <h4>
-        </h4>
+      </div>
+      <p className="developers">
+        Developers: Zhihao Lyu, Joseph Rodriguez, Mojtaba Yousefi, Haris Koutsopoulos
+      </p>
     </div>
   );
 };
