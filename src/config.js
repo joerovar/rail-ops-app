0732c3075693a6506c315c1b63cc82af50f36d92
Joseph Rodriguez <88448128+joerovar@users.noreply.github.com>
2024-06-27 12:48:40 -0400

    temporary host URL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 // src/config.js
 const API_URLS = {
-    ALL_RAIL: "https://bus-control-web-demo.ue.r.appspot.com/headway/all",
-    PUSH_RAIL: "https://bus-control-web-demo.ue.r.appspot.com/push",
+    ALL_RAIL: "https://bus-control-backend-w2rgrlldoq-uc.a.run.app/headway/all",
+    PUSH_RAIL: "https://bus-control-backend-w2rgrlldoq-uc.a.run.app/push",
+    // ALL_RAIL: "https://bus-control-web-demo.ue.r.appspot.com/headway/all",
+    // PUSH_RAIL: "https://bus-control-web-demo.ue.r.appspot.com/push",
   };
   
   export default API_URLS;
